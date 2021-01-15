fa3f7d1a5f9512099b0857dace530034fd37a267
drdevlin <drdevlin@fastmail.com>
2021-01-14 20:44:22 -0500

    Make startDate function pure

--- src/utils/__test__/dates.spec.js
+++ src/utils/__test__/dates.spec.js
@@ -1,18 +1,27 @@
 import { startDate } from '../dates.js';
 
 describe('Date Functions', () => {
-  describe('startDate(daysAgo)', () => {
-    it('accepts an integer argument', () => {
+  describe('startDate(dateNow, daysAgo)', () => {
+    it('accepts the Date.now() method for <dateNow>, positive integer for <daysAgo>', () => {
+      const dateNow = Date.now();
       const integer = 10;
-      expect(startDate(integer)).toBeDefined();
+      expect(startDate(dateNow, integer)).toBeDefined();
     });
     it('returns a string value', () => {
-      const output = startDate(10);
+      const output = startDate(Date.now(), 10);
       expect(typeof output).toBe('string');
     });
     it('returns a date string like this: yyyy-[m]m-[d]d', () => {
-      const output = startDate(10);
+      const output = startDate(Date.now(), 10);
       expect(output).toMatch(/^[0-9]{4}-[0-9]{1,2}-[0-9]{1,2}$/g);
+    });
+    it('returns the date so many <daysAgo> from <dateNow>', () => {
+      const mockToday = new Date('January 3, 2000').valueOf();
+      const daysAgo = 10;
+      const expectedOutput = '1999-12-24';
+
+      const actualOutput = startDate(mockToday, daysAgo);
+      expect(actualOutput).toStrictEqual(expectedOutput);
     });
   });
 });
--- src/utils/dates.js
+++ src/utils/dates.js
@@ -1,6 +1,8 @@
-export const startDate = (daysAgo) => {
-  const lengthOfDay = 86400000;
-  const startDate = new Date(Date.now() - (daysAgo * lengthOfDay));
-  const startDateFormatted = `${startDate.getFullYear()}-${startDate.getMonth() + 1}-${startDate.getDate()}`;
-  return startDateFormatted;
+export const startDate = (dateNow, daysAgo) => {
+  if (Number.isInteger(dateNow) && Number.isInteger(daysAgo) && daysAgo >= 0) {
+    const lengthOfDay = 86400000;
+    const startDate = new Date(dateNow - (daysAgo * lengthOfDay));
+    const startDateFormatted = `${startDate.getFullYear()}-${startDate.getMonth() + 1}-${startDate.getDate()}`;
+    return startDateFormatted;
+  }
 }
