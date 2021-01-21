b221a5afd8b081ea60bd0dc77aa52445dc099d58
drdevlin <drdevlin@fastmail.com>
2021-01-21 14:08:32 -0500

    Improve readability

--- src/App.test.js
+++ src/App.test.js
@@ -15,11 +15,14 @@ global.fetch = async () => {
 };
 
 describe('<App />', () => {
+
   it('displays a title', async () => {
     await render(<Provider store={store}><App /></Provider>);
     const title = screen.queryByRole('heading');
+
     expect(title).not.toBeNull();
   });
+
   it('displays feedback', async () => {
     await render(<Provider store={store}><App /></Provider>);
     const status = screen.queryAllByRole('status');
@@ -28,4 +31,5 @@ describe('<App />', () => {
 
     expect(either).toBeTruthy();
   });
+  
 });
